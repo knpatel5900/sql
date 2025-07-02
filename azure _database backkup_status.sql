cb3a03a5108a951ef872e693a1ee71c7590e4b43
Kishan Patel <kpatel@savanacorp.net>
2025-07-02 16:32:41 -0400

    update 07/02/2025

---
+++ azure _database backkup_status.sql
@@ -0,0 +1,14 @@
+	SELECT 
+    r.session_id,
+    r.command,
+    r.status,
+    r.percent_complete,
+    r.start_time,
+    r.estimated_completion_time / 1000 / 60 AS estimated_completion_time_minutes,
+    r.total_elapsed_time / 1000 / 60 AS elapsed_time_minutes,
+    r.logical_reads,
+    r.writes
+FROM 
+    sys.dm_exec_requests r
+WHERE 
+    r.command IN ('BACKUP DATABASE', 'BACKUP LOG');
