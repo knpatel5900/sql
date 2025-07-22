6da770840460eff5c6c75142d1d6f96863bb20c7
Kishan Patel <kpatel@savanacorp.net>
2025-07-22 15:56:15 -0400

    07-22-2025

--- new_tenant_user_pass_reset.sql
+++ new_tenant_user_pass_reset.sql
@@ -1,27 +1,25 @@
---New Tenant ::
-
---select * from  dbo.ossys_Tenant
---update dbo.ossys_Tenant set ESPACE_ID=15 where name ='bankingmaster' 
-
---Update Password
-
-
 DECLARE @tenant VARCHAR(100), 
         @tenantid VARCHAR(100),
         @prod   VARCHAR(100);
 
 -- Assign values to the variables
-SET @tenant = 'fiserv1';
-SET @prod = 'dev';
+SET @tenant = 'fbb2';
+SET @prod = 'uat';
 SET @tenantid = (SELECT TENANT_ID FROM dbo.OSUSR_K0H_BANKTENANT_SERVICESCONFIG where BANKNAME = @tenant);
 
----- Update query with concatenation
---update dbo.OSSYS_USER set PASSWORD='$1$eOW/xgnXwww1U2Gav2NLOyqIFPRdm4ODeK9Nqp1R23s=3DAB94C59435C82FEC4B30795574A3A7961CEABA9736136CC1474C219CEB6257B781A5C6809A1F25B83C8282648844C0F3A8ED1066314493A1BFB3A3FA1FA055' 
---where TENANT_ID= @tenantid and name IN (
---    'admin_' + @tenant + '_' + @prod,
---    'apiuser_' + @tenant + '_' + @prod,
---    'wfapiuser_' + @tenant + '_' + @prod
---);
+
+--New Tenant ::
+select ESPACE_ID,* from  dbo.ossys_Tenant where name = @tenant
+update dbo.ossys_Tenant set ESPACE_ID=14 where name = @tenant
+
+--Update Password
+-- Update query with concatenation
+update dbo.OSSYS_USER set PASSWORD='$1$Skeja4Z0l53DpenwSfWX1NUtwsPdoGLj2dGd4mGxETw=D6E826DF5A9626DAD108E06AFC1EFA463F6E13B0FC201902C31C82479E1C585AADD34EFBF448D8E95057FCDDF7C7B4CE8EDE7F0B75C7E5DD63E8EFEC2E976CBB' 
+where TENANT_ID= @tenantid and name IN (
+    'admin_' + @tenant + '_' + @prod,
+    'apiuser_' + @tenant + '_' + @prod,
+    'wfapiuser_' + @tenant + '_' + @prod
+);
 
 -- Select query with concatenation
 SELECT * 
@@ -31,3 +29,5 @@ WHERE name IN (
     'apiuser_' + @tenant + '_' + @prod,
     'wfapiuser_' + @tenant + '_' + @prod
 );
+
+select ESPACE_ID,* from  dbo.ossys_Tenant where name = @tenant
