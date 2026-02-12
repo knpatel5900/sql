96c05a02a0b9e7d42541cd67901afab384c1fa64
Kishan Patel <kpatel@savanacorp.net>
2026-02-12 13:01:06 -0500

    2/12/26

--- mssql/tenant_rename.sql
+++ mssql/tenant_rename.sql
@@ -2,7 +2,9 @@ DECLARE @Tenantname varchar(50) = 'Metropolitan Capital Bank'
 DECLARE @Tenantid int = 220
 
 --UPDATE  OSUSR_K0H_BANKTENANT_SERVICESCONFIG 
---SET BankName = @Tenantname
+--SET BankName = @Tenantname,
+--DOCUMENTSSERVICECONFIGNAME='test',
+--DOCUMENTSSERVICEENDPOINT='test'
 --WHERE Tenant_ID = @tenantid
 
 select * from OSUSR_K0H_BANKTENANT_SERVICESCONFIG WHERE Tenant_ID = @tenantid
@@ -13,5 +15,11 @@ WHERE ID = @tenantid
 select * from  dbo.ossys_Tenant
 WHERE ID = @tenantid
 
-
-
+--UPDATE  OSUSR_K0H_BANKTENANT_SERVICESCONFIG SET 
+--CUSTOMERSERVICEENDPOINT='test',
+--CUSTOMER_APIKEY='test',
+--CUSTOMER_APISECRET='test',
+--ACCOUNTSERVICEENDPOINT='test',
+--ACCOUNT_APIKEY='test',
+--ACCOUNT_APISECRET='test'
+--WHERE Tenant_ID = @tenantid
